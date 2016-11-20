8720ce601e7cdc954d184f4c96a4c89775b74bc5
I.Smal <serbov@pochta.ru>
2016-11-20 14:56:28 +0300

    mobile iphone SE is ready

--- app/js/script.js
+++ app/js/script.js
@@ -6,11 +6,25 @@ $('.js-slider').each(function (idx, item) {
 	$(this).slick({
 		slide: "#" + carouselId + " .js-slider-item",
 		appendArrows: "#" + carouselId + " .js-slider-nav",
-		prevArrow: '<button type="button" class="js-slider-prev">prev</button>',
-		nextArrow: '<button type="button" class="js-slider-next">next</button>'
+		prevArrow: '<button type="button" class="slider-nav__prev js-slider-prev">prev</button>',
+		nextArrow: '<button type="button" class="slider-nav__next js-slider-next">next</button>',
+		infinite: false
 	});
 });
 
 //formstyler
 $('.js-select select').styler();
+
+//scrollbar
+$('.jq-selectbox__dropdown ul').enscroll({
+	verticalTrackClass: 'scrollbar__track',
+	verticalHandleClass: 'scrollbar__handle',
+	drawScrollButtons: true,
+	scrollUpButtonClass: 'scrollbar__up',
+	scrollDownButtonClass: 'scrollbar__down'
+});
+
+//inputmask
+$(".js-tel").mask("+7 ( 9 9 9 ) 9 9 9 - 9 9 - 9 9");
+
 /*/script*/

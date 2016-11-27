d22f4b9f721e1318246feca31a7ff0c352a03225
I.Smal <serbov@pochta.ru>
2016-11-27 22:25:05 +0300

    html, css, js are almost ready

--- js/script.js
+++ js/script.js
@@ -8,8 +8,6 @@ $('.js-slider').each(function (idx, item) {
 	var carouselId = "slider-" + idx;
 	this.id = carouselId;
 
-	// alert(window.devicePixelRatio < 2 && ((screen.width >= 768 && screen.height >= 1024) || (screen.width >= 1024 && screen.height >= 768)) && $(this).hasClass('js-slider-2'));
-
 	if(window.devicePixelRatio < 2 && ((window.innerWidth >= 768 && window.innerWidth < 1280) && $(this).hasClass('js-slider-2'))) {
 		$(this).slick({
 			slide: "#" + carouselId + " .js-slider-item",
@@ -80,4 +78,76 @@ $('.js-btn-login').on('click', function (e) {
 	$('.js-login').removeClass('hide');
 });
 
+//cite
+$('.js-logo').on('click', function (e) {
+	$('.js-logo-cite').removeClass('hide');
+});
+
+//slider with repair places change
+if($('.js-places-slider').length) {
+	var sliderPlaces = $('.js-places-slider');
+	sliderPlaces.slick({
+		slide: ".js-places-slider .js-slider-item",
+		appendArrows: ".js-places-slider .js-slider-nav",
+		prevArrow: '<button type="button" class="slider-nav__prev js-slider-prev">prev</button>',
+		nextArrow: '<button type="button" class="slider-nav__next js-slider-next">next</button>',
+		slidesToShow: 1,
+		autoplay: true
+	});
+
+	var sliderPlacesArray = ["в спортзале", "дома", "в офисе"];
+	var sliderPlacesText = $('.js-places-text');
+
+	sliderPlaces.on('afterChange', function(event, slick, currentSlide, nextSlide){
+		var currentSlide = $('.js-places-slider .slick-current');
+
+		if(currentSlide.hasClass('intro-slider__gym')) {
+			sliderPlacesText.text(sliderPlacesArray[0]);
+		} else
+		if(currentSlide.hasClass('intro-slider__home')) {
+			sliderPlacesText.text(sliderPlacesArray[1]);
+		} else
+		if(currentSlide.hasClass('intro-slider__office')) {
+			sliderPlacesText.text(sliderPlacesArray[2]);
+		}
+	});
+}
+
+//prices slider
+if($('.js-prices-slider').length) {
+	var sliderPrices = $('.js-prices-slider');
+	sliderPrices.slick({
+		slide: ".js-prices-slider .js-slider-item",
+		appendArrows: ".js-prices-slider .js-slider-nav",
+		prevArrow: '<button type="button" class="slider-nav__prev js-slider-prev">prev</button>',
+		nextArrow: '<button type="button" class="slider-nav__next js-slider-next">next</button>',
+		asForNav: '.js-prices-nav',
+		infinite: false,
+		slidesToShow: 1
+	});
+
+	var pricesNavDot = $('.js-prices-nav a');
+	pricesNavDot.on('click', function (e) {
+		e.preventDefault();
+		sliderPrices.slick('slickGoTo', $(this).parent().index());
+	});
+}
+
+//prices info
+if($('.js-defect-points').length) {
+	var defectPoint = $('.js-defect-points li');
+	var defectInfo = $('.js-defect-info .two-columns');
+
+	defectPoint.hover(
+		function() {
+			defectInfo.hide();
+			$(defectInfo[$(this).index()]).show();
+			$(defectInfo[$(this).index()]).children('p').show();
+		},
+		function() {
+			defectInfo.show();
+			$(defectInfo[$(this).index()]).children('p').hide();
+		}
+	);
+}
 /*/script*/
